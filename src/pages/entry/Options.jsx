1b0e6ab09546d1a6c224577359b3812380e3a89d
jmarcossf <jamlmarcos@gmail.com>
2022-09-15 19:20:45 -0700

    Code and test scoop options images from the server.

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -8,24 +8,24 @@ export default function Options({ optionType }) {
 
   // optionType is "scoops" or "toppings"
   useEffect(() => {
-    console.log('optionType', optionType)
+    console.log("optionType", optionType);
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then(response => {
         setItems(response.data);
-        console.log('response!!!!', response.data)
+        console.log("response!!!!", response.data);
       })
       .catch(error => {
-          console.log('HERE!!!!', error)
+        console.log("HERE!!!!", error);
         // TODO: handle error response
       });
   }, [optionType]);
-  console.log('items:', items)
+  console.log("items:", items);
 
   // TODO: replace the null ref with ToppingOption once available
   const ItemComponent = optionType === "scoops" ? ScoopOptions : null;
 
   const optionItems = items.map(item => <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />);
 
-    return <Row>{ optionItems }</Row>
+  return <Row>{optionItems}</Row>;
 }
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Options from "../Options";
 import { makeServer } from "../../../server";
